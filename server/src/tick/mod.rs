5ac18bb9e25fe4beb3e51173b59d0d74d181639e
João Xavier <fastjonh@gmail.com>
2025-06-22 14:57:47 -0300

    spliting server processing

--- server/src/tick/mod.rs
+++ server/src/tick/mod.rs
@@ -8,24 +8,28 @@ pub struct Plugin;
 impl bevy::prelude::Plugin for Plugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(Time::<Fixed>::from_hz(128.0))
-            .add_systems(FixedUpdate, update)
-            .add_systems(FixedUpdate, move_players)
-            .add_systems(FixedUpdate, sync_players);
+            .add_systems(FixedUpdate, handle_connectivity)
+            .add_systems(FixedUpdate, handle_player_input)
+            .add_systems(FixedUpdate, physx_tick)
+            .add_systems(FixedUpdate, send_players_pos);
     }
 }
 
-fn move_players(mut query: Query<(&mut Transform, &PlayerInput)>, time: Res<Time>) {
+fn physx_tick(mut query: Query<(&mut Transform, &PlayerInput)>, time: Res<Time>) {
     for (mut transform, input) in query.iter_mut() {
         let x = (input.right as i8 - input.left as i8) as f32;
         let y = (input.down as i8 - input.up as i8) as f32;
+
         let adjustment = if x != 0. && y != 0. { 0.7 } else { 1.0 };
+
         transform.translation.x += x * PLAYER_MOVE_SPEED * adjustment * time.delta().as_secs_f32();
         transform.translation.z += y * PLAYER_MOVE_SPEED * adjustment * time.delta().as_secs_f32();
     }
 }
 
-fn sync_players(mut server: ResMut<RenetServer>, query: Query<(&Transform, &Player)>) {
+fn send_players_pos(mut server: ResMut<RenetServer>, query: Query<(&Transform, &Player)>) {
     let mut players: HashMap<ClientId, [f32; 3]> = HashMap::new();
+
     for (transform, player) in query.iter() {
         players.insert(player.id, transform.translation.into());
     }
@@ -35,7 +39,24 @@ fn sync_players(mut server: ResMut<RenetServer>, query: Query<(&Transform, &Play
     server.broadcast_message(DefaultChannel::Unreliable, sync_message);
 }
 
-fn update(
+fn handle_player_input(
+    mut commands: Commands,
+    mut server: ResMut<RenetServer>,
+    lobby: ResMut<Lobby>,
+) {
+    for client_id in server.clients_id() {
+        while let Some(message) = server.receive_message(client_id, DefaultChannel::ReliableOrdered)
+        {
+            let player_input: PlayerInput = data::decode(&message);
+
+            if let Some(player_entity) = lobby.players.get(&client_id) {
+                commands.entity(*player_entity).insert(player_input);
+            }
+        }
+    }
+}
+
+fn handle_connectivity(
     mut server_events: EventReader<ServerEvent>,
     mut commands: Commands,
     mut lobby: ResMut<Lobby>,
@@ -47,9 +68,9 @@ fn update(
                 info!("Player {} connected.", client_id);
                 // Spawn player cube
                 let player_entity = commands
-                    .spawn((Transform::from_xyz(0.0, 0.5, 0.0),))
+                    .spawn(Player { id: *client_id })
                     .insert(PlayerInput::default())
-                    .insert(Player { id: *client_id })
+                    .insert(Transform::from_xyz(0.0, 0.5, 0.0))
                     .id();
 
                 // We could send an InitState with all the players id and positions for the client
@@ -74,17 +95,6 @@ fn update(
                 let message = data::encode(ServerMessage::PlayerDisconnected { id: *client_id });
 
                 server.broadcast_message(DefaultChannel::ReliableOrdered, message);
-            }
-        }
-    }
-
-    for client_id in server.clients_id() {
-        while let Some(message) = server.receive_message(client_id, DefaultChannel::ReliableOrdered)
-        {
-            let player_input: PlayerInput = data::decode(&message);
-
-            if let Some(player_entity) = lobby.players.get(&client_id) {
-                commands.entity(*player_entity).insert(player_input);
             }
         }
     }
